026dde893a68c7bc9ded2d47f1b2fcc341bd832b
Nick <zhong.ren@hotmail.com>
2020-07-27 11:23:37 -0400

    EnableWebMvc

--- src/main/java/org/atavdb/controller/Config.java
+++ src/main/java/org/atavdb/controller/Config.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.view.JstlView;
  * @author nick
  */
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = {"org.atavdb.controller"})
 public class Config implements WebMvcConfigurer {
 
