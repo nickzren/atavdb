2eac77de0f2d294d43f7f6a66b9fc7e3a4502449
Nick <zhong.ren@hotmail.com>
2021-01-21 10:56:33 -0500

    add Google Analytics Tracking

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(public router: Router) {
+    // Google Analytics track the URL a user has navigated to
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'UA-32885253-12', { 'page_path': event.urlAfterRedirects });
+      }
+    })
+  }
 }
